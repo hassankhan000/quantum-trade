ed5f2af0c6fd7354049d4dad9c80f088627aa891
Muhammad Huzaifa Waseem <muhammadhuzaifakhan2233@gmail.com>
2024-05-11 11:31:37 +0500

    UPFF

--- index.php
+++ index.php
@@ -5,63 +5,38 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-
 function installedPath()
 {
     return 'core/storage/LICENCE.txt';
 }
 
+// Check if the application is installed by checking the existence of LICENCE.txt
+// If not installed, redirect to the installation page
 if (!file_exists(installedPath())) {
     header('Location: install/index.php');
-    die();
-};
+    exit(); // Use exit instead of die for consistency
+}
 
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists(__DIR__ . '/core/storage/framework/maintenance.php')) {
+// Check if the application is under maintenance
+if (file_exists(__DIR__ . '/core/storage/framework/down')) {
     require __DIR__ . '/core/storage/framework/maintenance.php';
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
+// Register The Auto Loader
 require __DIR__ . '/core/vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
+// Run The Application
 $app = require_once __DIR__ . '/core/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
-$response = tap($kernel->handle(
+// Handle the incoming request using the application's HTTP kernel
+$response = $kernel->handle(
     $request = Request::capture()
-))->send();
+);
 
+// Send the response back to the client's browser
+$response->send();
+
+// Terminate the request
 $kernel->terminate($request, $response);
