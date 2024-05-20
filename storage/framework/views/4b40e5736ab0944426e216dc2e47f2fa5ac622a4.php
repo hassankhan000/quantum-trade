470bf72211b9d38b351b2bd7b52d5d1e634f654a
Muhammad Huzaifa Waseem <muhammadhuzaifakhan2233@gmail.com>
2024-05-21 02:09:46 +0500

    up

--- storage/framework/views/4b40e5736ab0944426e216dc2e47f2fa5ac622a4.php
+++ storage/framework/views/4b40e5736ab0944426e216dc2e47f2fa5ac622a4.php
@@ -72,7 +72,7 @@
         <nav class="menu-box">
             <div class="close-btn"><i class="fas fa-times"></i></div>
             <div class="nav-logo">
-                <a href="/"><img src="<?php echo e(asset('asset/theme4/web_assets/assets/img/logo/logo.png')); ?>"
+                <a href="index.html"><img src="<?php echo e(asset('asset/theme4/web_assets/assets/img/logo/logo.png')); ?>"
                         style="height: 35px;" alt="Logo"></a>
             </div>
             <div class="menu-outer">
