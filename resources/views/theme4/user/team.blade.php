10c992ad6b1230f728092a26f9a4b2f176156dbb
Muhammad Huzaifa Waseem <muhammadhuzaifakhan2233@gmail.com>
2024-05-22 20:41:22 +0500

    update

--- resources/views/theme4/user/team.blade.php
+++ resources/views/theme4/user/team.blade.php
@@ -12,6 +12,130 @@
         .border-success {
             border-color: #aef32d !important;
         }
+
+        /*Now the CSS*/
+
+        .tree ul {
+            padding-top: 20px;
+            position: relative;
+
+            transition: all 0.5s;
+            -webkit-transition: all 0.5s;
+            -moz-transition: all 0.5s;
+        }
+
+        .tree li {
+            float: left;
+            text-align: center;
+            list-style-type: none;
+            position: relative;
+            padding: 20px 5px 0 5px;
+
+            transition: all 0.5s;
+            -webkit-transition: all 0.5s;
+            -moz-transition: all 0.5s;
+        }
+
+        /*We will use ::before and ::after to draw the connectors*/
+
+        .tree li::before,
+        .tree li::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 50%;
+            border-top: 1px solid #aef32d;
+            width: 50%;
+            height: 20px;
+        }
+
+        .tree li::after {
+            right: auto;
+            left: 50%;
+            border-left: 1px solid #aef32d;
+        }
+
+        /*We need to remove left-right connectors from elements without
+                                                any siblings*/
+        .tree li:only-child::after,
+        .tree li:only-child::before {
+            display: none;
+        }
+
+        /*Remove space from the top of single children*/
+        .tree li:only-child {
+            padding-top: 0;
+        }
+
+        /*Remove left connector from first child and
+                                                right connector from last child*/
+        .tree li:first-child::before,
+        .tree li:last-child::after {
+            border: 0 none;
+        }
+
+        /*Adding back the vertical connector to the last nodes*/
+        .tree li:last-child::before {
+            border-right: 1px solid #aef32d;
+            border-radius: 0 5px 0 0;
+            -webkit-border-radius: 0 5px 0 0;
+            -moz-border-radius: 0 5px 0 0;
+        }
+
+        .tree li:first-child::after {
+            border-radius: 5px 0 0 0;
+            -webkit-border-radius: 5px 0 0 0;
+            -moz-border-radius: 5px 0 0 0;
+        }
+
+        /*Time to add downward connectors from parents*/
+        .tree ul ul::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 50%;
+            border-left: 1px solid #aef32d;
+            width: 0;
+            height: 20px;
+        }
+
+        .tree li a {
+            border: 1px solid #aef32d;
+            padding: 5px 10px;
+            text-decoration: none;
+            color: #666;
+            font-family: arial, verdana, tahoma;
+            font-size: 11px;
+            display: inline-block;
+
+            border-radius: 5px;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+
+            transition: all 0.5s;
+            -webkit-transition: all 0.5s;
+            -moz-transition: all 0.5s;
+        }
+
+        /*Time for some hover effects*/
+        /*We will apply the hover effect the the lineage of the element also*/
+        .tree li a:hover,
+        .tree li a:hover+ul li a {
+            background: #c8e4f8;
+            color: #000;
+            border: 1px solid #94a0b4;
+        }
+
+        /*Connector styles on hover*/
+        .tree li a:hover+ul li::after,
+        .tree li a:hover+ul li::before,
+        .tree li a:hover+ul::before,
+        .tree li a:hover+ul ul::before {
+            border-color: #94a0b4;
+        }
+
+        /*Thats all. I hope you enjoyed it.
+                                                Thanks :)*/
     </style>
     <div class="nk-content nk-content-fluid">
         <div class="container-xl wide-xl">
@@ -165,6 +289,81 @@
                                     </div>
                                 </div>
                             </div>
+                            @php
+                                $reference = auth()->user()->refferals;
+
+                            @endphp
+                            @php
+                                $reference = auth()->user()->refferals;
+                            @endphp
+
+                            @if ($reference->count() > 0)
+                                {{-- <div class="tree">
+                                    <div class="node m-0">
+                                        {{ auth()->user()->full_name . ' - ' . currentPlan(auth()->user()) }}
+                                    </div>
+                                    <div class="children">
+                                        @foreach ($reference as $user)
+                                            <div class="node">
+                                                {{ $user->full_name . ' - ' . currentPlan($user) }}
+                                            </div>
+                                            <div class="children">
+                                                @foreach ($user->refferals as $ref)
+                                                    <div class="node">
+                                                        {{ $ref->full_name . ' - ' . currentPlan($ref) }}
+                                                    </div>
+                                                    <div class="children">
+                                                        @foreach ($ref->refferals as $ref2)
+                                                            <div class="node">
+                                                                {{ $ref2->full_name . ' - ' . currentPlan($ref2) }}
+                                                            </div>
+                                                        @endforeach
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div> --}}
+
+                                <h5 class="mb-0 text-light text-center fw-bold">{{ __('Reference Tree') }}</h5>
+
+                                <div class="tree pb-5">
+                                    <ul>
+                                        <li>
+                                            <a
+                                                href="#">{{ auth()->user()->full_name . ' - ' . currentPlan(auth()->user()) }}</a>
+                                            <ul>
+                                                @foreach ($reference as $user)
+                                                    <li>
+                                                        <a
+                                                            href="#">{{ $user->full_name . ' - ' . currentPlan($user) }}</a>
+                                                        <ul>
+                                                            @foreach ($user->refferals as $ref)
+                                                                <li>
+                                                                    <a
+                                                                        href="#">{{ $ref->full_name . ' - ' . currentPlan($ref) }}</a>
+                                                                    <ul>
+                                                                        @foreach ($ref->refferals as $ref2)
+                                                                            <li>
+                                                                                <a href="#">
+                                                                                    {{ $ref2->full_name . ' - ' . currentPlan($ref2) }}</a>
+                                                                            </li>
+                                                                        @endforeach
+                                                                    </ul>
+                                                                </li>
+                                                            @endforeach
+                                                        </ul>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            @else
+                                <p class="mt-2">
+                                    {{ __('No Reference User Found') }}
+                                </p>
+                            @endif
                         </div>
                     </div>
                 </div>
--- storage/framework/views/616ec8d853a481f28db9187c3d3ae5ad76e6be73.php
+++ storage/framework/views/616ec8d853a481f28db9187c3d3ae5ad76e6be73.php
@@ -11,6 +11,130 @@
         .border-success {
             border-color: #aef32d !important;
         }
+
+        /*Now the CSS*/
+
+        .tree ul {
+            padding-top: 20px;
+            position: relative;
+
+            transition: all 0.5s;
+            -webkit-transition: all 0.5s;
+            -moz-transition: all 0.5s;
+        }
+
+        .tree li {
+            float: left;
+            text-align: center;
+            list-style-type: none;
+            position: relative;
+            padding: 20px 5px 0 5px;
+
+            transition: all 0.5s;
+            -webkit-transition: all 0.5s;
+            -moz-transition: all 0.5s;
+        }
+
+        /*We will use ::before and ::after to draw the connectors*/
+
+        .tree li::before,
+        .tree li::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            right: 50%;
+            border-top: 1px solid #aef32d;
+            width: 50%;
+            height: 20px;
+        }
+
+        .tree li::after {
+            right: auto;
+            left: 50%;
+            border-left: 1px solid #aef32d;
+        }
+
+        /*We need to remove left-right connectors from elements without
+                                                any siblings*/
+        .tree li:only-child::after,
+        .tree li:only-child::before {
+            display: none;
+        }
+
+        /*Remove space from the top of single children*/
+        .tree li:only-child {
+            padding-top: 0;
+        }
+
+        /*Remove left connector from first child and
+                                                right connector from last child*/
+        .tree li:first-child::before,
+        .tree li:last-child::after {
+            border: 0 none;
+        }
+
+        /*Adding back the vertical connector to the last nodes*/
+        .tree li:last-child::before {
+            border-right: 1px solid #aef32d;
+            border-radius: 0 5px 0 0;
+            -webkit-border-radius: 0 5px 0 0;
+            -moz-border-radius: 0 5px 0 0;
+        }
+
+        .tree li:first-child::after {
+            border-radius: 5px 0 0 0;
+            -webkit-border-radius: 5px 0 0 0;
+            -moz-border-radius: 5px 0 0 0;
+        }
+
+        /*Time to add downward connectors from parents*/
+        .tree ul ul::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 50%;
+            border-left: 1px solid #aef32d;
+            width: 0;
+            height: 20px;
+        }
+
+        .tree li a {
+            border: 1px solid #aef32d;
+            padding: 5px 10px;
+            text-decoration: none;
+            color: #666;
+            font-family: arial, verdana, tahoma;
+            font-size: 11px;
+            display: inline-block;
+
+            border-radius: 5px;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+
+            transition: all 0.5s;
+            -webkit-transition: all 0.5s;
+            -moz-transition: all 0.5s;
+        }
+
+        /*Time for some hover effects*/
+        /*We will apply the hover effect the the lineage of the element also*/
+        .tree li a:hover,
+        .tree li a:hover+ul li a {
+            background: #c8e4f8;
+            color: #000;
+            border: 1px solid #94a0b4;
+        }
+
+        /*Connector styles on hover*/
+        .tree li a:hover+ul li::after,
+        .tree li a:hover+ul li::before,
+        .tree li a:hover+ul::before,
+        .tree li a:hover+ul ul::before {
+            border-color: #94a0b4;
+        }
+
+        /*Thats all. I hope you enjoyed it.
+                                                Thanks :)*/
     </style>
     <div class="nk-content nk-content-fluid">
         <div class="container-xl wide-xl">
@@ -164,6 +288,57 @@
                                     </div>
                                 </div>
                             </div>
+                            <?php
+                                $reference = auth()->user()->refferals;
+
+                            ?>
+                            <?php
+                                $reference = auth()->user()->refferals;
+                            ?>
+
+                            <?php if($reference->count() > 0): ?>
+                                
+
+                                <h5 class="mb-0 text-light text-center fw-bold"><?php echo e(__('Reference Tree')); ?></h5>
+
+                                <div class="tree pb-5">
+                                    <ul>
+                                        <li>
+                                            <a
+                                                href="#"><?php echo e(auth()->user()->full_name . ' - ' . currentPlan(auth()->user())); ?></a>
+                                            <ul>
+                                                <?php $__currentLoopData = $reference; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                    <li>
+                                                        <a
+                                                            href="#"><?php echo e($user->full_name . ' - ' . currentPlan($user)); ?></a>
+                                                        <ul>
+                                                            <?php $__currentLoopData = $user->refferals; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ref): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                                <li>
+                                                                    <a
+                                                                        href="#"><?php echo e($ref->full_name . ' - ' . currentPlan($ref)); ?></a>
+                                                                    <ul>
+                                                                        <?php $__currentLoopData = $ref->refferals; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $ref2): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                                            <li>
+                                                                                <a href="#">
+                                                                                    <?php echo e($ref2->full_name . ' - ' . currentPlan($ref2)); ?></a>
+                                                                            </li>
+                                                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                                                    </ul>
+                                                                </li>
+                                                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                                        </ul>
+                                                    </li>
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            <?php else: ?>
+                                <p class="mt-2">
+                                    <?php echo e(__('No Reference User Found')); ?>
+
+                                </p>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
